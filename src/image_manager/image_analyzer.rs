d112195bd14b30ca7e61ef4b98d5730d5ff79f4c
hfjxjjd123 <licbusiness21@gmail.com>
2023-08-11 21:46:03 +0900

    analyize to find query, apply button

--- src/image_manager/image_analyzer.rs
+++ src/image_manager/image_analyzer.rs
@@ -5,6 +5,14 @@ use image::GenericImageView;
 const STEP0_WIDTH: f64 = 0.9565;
 const STEP0_HEIGHT: f64 = 0.064;
 
+const STEP_APPLY_WIDTH: f64 = 0.071;
+const STEP_APPLY_HEIGHT_DEFAULT: f64 = 0.3148;
+const STEP_APPLY_HEIGHT_ADD: f64 = 0.03;
+
+const STEP_QUERY_WIDTH: f64 = 0.9055;
+const STEP_QUERY_HEIGHT: f64 = 0.2429;
+
+
 pub fn analysis_by_color()->(u32, u32){
     let screenshot_path = "screenshot.png";
     let screenshot_image = image::open(screenshot_path);
@@ -42,4 +50,23 @@ pub fn get_absolute_pixel(canvas_element: ElementRect, x: u32, y: u32)->(f64, f6
     let abs_y = canvas_element.y + y as f64;
     println!("canvas = {:?}, {:?}", abs_x, abs_y);
     (abs_x, abs_y)
+}
+
+pub fn query_button_location() -> (i64, i64){
+    let screenshot_path = "screenshot2.png";
+    let screenshot_image = image::open(screenshot_path);
+
+    let binding = screenshot_image.expect("REASON");
+
+    (((binding.width()) as f64 * STEP_QUERY_WIDTH) as i64, ((binding.height()) as f64 * STEP_QUERY_HEIGHT) as i64)
+
+}
+pub fn apply_button_location(index: i32) -> (i64, i64){
+    let screenshot_path = "screenshot2.png";
+    let screenshot_image = image::open(screenshot_path);
+
+    let binding = screenshot_image.expect("REASON");
+
+    (((binding.width()) as f64 * STEP_APPLY_WIDTH) as i64, ((binding.height()) as f64 * (STEP_APPLY_HEIGHT_DEFAULT + (index as f64 * STEP_APPLY_HEIGHT_ADD))) as i64)
+
 }
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ async fn main() -> WebDriverResult<()> {
     let driver: WebDriver = start_driver().await?;
     //while initialize 값이 에러가 아닐 동안 계속 트라이
     initialize(&driver).await?;
-
     initial_apply_canvas(&driver).await?;
 
     tokio::time::sleep(Duration::from_secs(10)).await;
