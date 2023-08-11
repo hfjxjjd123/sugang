bbcddacc0b5c61eb5a544e208579caa4cb4c3a5b
hfjxjjd123 <licbusiness21@gmail.com>
2023-08-12 01:54:36 +0900

    Error 핸들링만 남기고 완료

--- src/iter_apply.rs
+++ src/iter_apply.rs
@@ -4,8 +4,9 @@ use crate::image_manager::image_screenshot::screenshot_canvas;
 use crate::image_manager::image_analyzer::*;
 use crate::image_manager::pass_iframe::*;
 
-pub async fn initial_apply_canvas(driver: &WebDriver) -> WebDriverResult<()>{
-    
+pub async fn initial_apply_canvas(driver: &WebDriver, target_index: Vec<i32>) -> WebDriverResult<(i64, i64)>{
+    let mut coordinates: Vec<(i64, i64)> = vec![];
+
     let iframe1_location = pass_iframe1(&driver).await?;
     driver.enter_frame(0).await?;
     let iframe2_location = pass_iframe2(&driver).await?;
@@ -13,23 +14,20 @@ pub async fn initial_apply_canvas(driver: &WebDriver) -> WebDriverResult<()>{
 
     let apply_canvas = screenshot_canvas(&driver, "TmtViewer", "screenshot2.png").await?;
 
-    // let query_button = query_button_location();
-    let apply_button2 = apply_button_location(2);
+    for index in target_index{
+        let apply_button = apply_button_location(index);
+        let final_x:i64 = (apply_canvas.x + apply_button.0 as f64 + iframe1_location.x + iframe2_location.x) as i64;
+        let final_y:i64 = (apply_canvas.y + apply_button.1 as f64 + iframe1_location.y + iframe2_location.y) as i64;
 
+        coordinates.push((final_x, final_y));
+    }
 
-    let final_x:i64 = (apply_canvas.x + apply_button2.0 as f64 + iframe1_location.x + iframe2_location.x) as i64;
-    let final_y:i64 = (apply_canvas.y + apply_button2.1 as f64 + iframe1_location.y + iframe2_location.y) as i64;
+    let query_button = query_button_location();
+    let query_button_x = (apply_canvas.x + query_button.0 as f64) as i64;
+    let query_button_y = (apply_canvas.y + query_button.1 as f64) as i64;
+    coordinates.push((query_button_x, query_button_y));
 
-    println!("{},{}", final_x, final_y);
-
-
-    driver
-    .action_chain()
-    .move_to(final_x, final_y)
-    .click()
-    .perform()
-    .await?;
-
-    Ok(())
+    Ok(coordinates)
 
 }
+
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,29 @@ use sugang::iter_apply::*;
 
 #[tokio::main]
 async fn main() -> WebDriverResult<()> {
+    let num_of_basket: Vec<i32> = vec![1,2,6];
+
     let driver: WebDriver = start_driver().await?;
-    //while initialize 값이 에러가 아닐 동안 계속 트라이
     initialize(&driver).await?;
-    initial_apply_canvas(&driver).await?;
+
+    let targets: Vec<(i64, i64)> = initial_apply_canvas(&driver, num_of_basket).await?;
+    let reload_button = targets.0;
+
+    //성공했다면 성공했을시 로직
+    while true{
+        for target in targets{
+            driver.action_chain()
+                .move_to(target.0, target.1)
+                .click()
+                .perform()
+                .await?;
+            //기다렸다가 실행하는 로직 or alert 핸들링하기
+            //alert가 없는데 alert 핸들러 실행하는 로직 수정
+            alert_handler(&driver);
+        }
+        tokio::time::sleep(Duration::from_secs(2)).await;
+    }
+    
 
     tokio::time::sleep(Duration::from_secs(10)).await;
     terminate_driver(driver).await?;
