ddfee6ec4a5ecdc83fb5d080d236886f321fbdfa
hfjxjjd123 <licbusiness21@gmail.com>
2023-08-13 13:19:19 +0900

    Alert 창이 없는 에러시 Alert Handler 그냥 넘기기

--- src/web_exception_handler.rs
+++ src/web_exception_handler.rs
@@ -1,8 +1,13 @@
-use thirtyfour::error::WebDriverResult;
+use thirtyfour::error::*;
 use thirtyfour::WebDriver;
 
 
 pub async fn alert_handler(driver: &WebDriver) -> WebDriverResult<()> {
-    driver.accept_alert().await?;
-    Ok(())
+    let close_alert = driver.accept_alert().await;
+
+    match close_alert{
+        Ok(_) => Ok(()),
+        Err(WebDriverError::NoSuchAlert(_)) => Ok(()),
+        _ => Err(WebDriverError::CustomError("Unexpected".to_owned())),
+    }
 }
